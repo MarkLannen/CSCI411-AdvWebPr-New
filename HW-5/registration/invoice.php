17309ccb53e83a641ec2b89f2f284788dd25f48f
MarkLannen <marklannendev@gmail.com>
2020-02-24 19:46:48 -0700

    added header

--- HW-5/registration/invoice.php
+++ HW-5/registration/invoice.php
@@ -4,6 +4,18 @@ require('fpdf.php');
 class invoicePDF extends FPDF {
     function header() {
         $this -> Image('../images/ppp-logo-150px.png');
+        $this -> SetFont('Arial','B',24);
+        $this -> Cell(300, -50, 'Invoice', 0,0, 'C');
+        $this -> Ln(20);
+        $this -> SetFont('Arial','B',12);
+        $this -> Cell(287, -68, '1234 Industrial Way', 0,0, 'C');
+        $this -> Ln(20);
+        $this -> SetFont('Arial','B',12);
+        $this -> Cell(300, -95, 'Missoula, MT', 0,0, 'C');
+        $this -> Ln(20);
+        $this -> SetFont('Arial','B',12);
+        $this -> Cell(300, -122, '123-456-7890', 0,0, 'C');
+
     }
 }
 
