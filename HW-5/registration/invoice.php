c2ab2265204b375d5606e14911ee06f73e97cd2f
MarkLannen <marklannendev@gmail.com>
2020-02-24 20:17:19 -0700

    passed data from form

--- HW-5/registration/invoice.php
+++ HW-5/registration/invoice.php
@@ -2,7 +2,14 @@
 require('fpdf.php');
 
 class invoicePDF extends FPDF {
+
+
+
+
     function header() {
+
+
+
         $this -> Image('../images/ppp-logo-150px.png');
         $this -> SetFont('Arial','B',24);
         $this -> Cell(300, -50, 'Invoice', 0,0, 'C');
@@ -17,14 +24,33 @@ class invoicePDF extends FPDF {
         $this -> Cell(300, -122, '123-456-7890', 0,0, 'C');
 
     }
+
+    function headerTable() {
+        $this -> Ln(20);
+        $this -> SetFont('Arial','B',12);
+        $this -> Cell(20, 10, 'Product', 1,0, 'C');
+    }
+
+    function inputs($firstName) {
+        $this -> Ln(20);
+        $this -> SetFont('Arial','B',12);
+        $this -> Cell(20, 10, $firstName, 1,0, 'C');
+    }
+
+
+
+
 }
 
 
 
 
+$firstName = $_POST["firstName"];
 
 $pdf = new invoicePDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+//$pdf->SetFont('Arial','B',16);
+//$pdf->Cell(40,10,'Hello World!');
+$pdf->headerTable();
+$pdf->inputs($firstName);
 $pdf->Output();
