b9cbe1f909dcb6b7c92abbb6de6ff09394e75cf5
MarkLannen <marklannendev@gmail.com>
2020-03-10 20:54:06 -0600

    added checkboxes to products

---
+++ Midterm/dbConnection.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("ml120499@webdev.cs.umt.edu","ml120499","pohthaimooShohnai0chaisah5Aexo","ml120499");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+?>
