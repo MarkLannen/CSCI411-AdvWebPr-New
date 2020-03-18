7ec5cbe50e9e50198c400e9377a75049f9160c49
MarkLannen <marklannendev@gmail.com>
2020-03-17 18:14:22 -0600

    commit

--- Midterm/dbConnection.php
+++ Midterm/dbConnection.php
@@ -1,9 +1,25 @@
 <?php
-$con = mysqli_connect("ml120499@webdev.cs.umt.edu","ml120499","pohthaimooShohnai0chaisah5Aexo","ml120499");
+//$con = mysqli_connect("ml120499@webdev.cs.umt.edu","ml120499","pohthaimooShohnai0chaisah5Aexo","ml120499");
+//
+//// Check connection
+//if (mysqli_connect_errno()) {
+//    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//    exit();
+//}
+////?>
+
+<?php
+$servername = "webdev.cs.umt.edu";
+$username = "ml120499";
+$password = "Udana-6";
+$dbname = "ml120499";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
